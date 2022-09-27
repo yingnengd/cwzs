b9249cfbb3dcd6efeb6c7b76fa0a7e7cc4578ab4
yingnengd <704441474@qq.com>
2022-09-27 21:06:55 +0800

    Signed-off-by: yingnengd <704441474@qq.com> 1

---
+++ wxFlask.py
@@ -0,0 +1,46 @@
+# -- coding: utf-8 --**
+from wxsb import sbcw
+from flask import Flask
+import time
+import random
+import traceback2 as traceback
+import os
+
+from flask_apscheduler import APScheduler
+       
+m = random.randint(1,3)
+s = random.randint(1,20)
+
+class Config(object):  # 创建配置，用类
+    JOBS = [  # 任务列表
+        {  # 任务字典（细节）
+            'id': 'index',
+            'func': '__main__:index',
+            # 'args': (1, 2),
+            'trigger': 'cron',
+            'timezone':"Asia/Shanghai",
+            'day_of_week':'mon-sun',
+            'hour':'19',
+            'minute':'{}'.format(m),
+            'second':'{}'.format(s)
+        }
+    ]
+
+b = sbcw()#实例化（）不应在其他函数里，实例化后的实例driver应用在其他函数里    
+app = Flask(__name__)  # 实例化flask
+app.config.from_object(Config())  # 为实例化的flask引入配置
+@app.route('/')  # 首页路由
+def index():  # 首页视图函数，就返回个hello
+    try:
+        b.send_msg()
+        time.sleep(random.uniform(1,3))
+    except Exception as e:
+        print(e.args)
+        print('======')
+        print(traceback.format_exc())
+
+if __name__ == '__main__':
+    scheduler = APScheduler()  # 实例化APScheduler
+    scheduler.init_app(app)  # 把任务列表放进flask
+    scheduler.start()  # 启动任务列表
+    app.run(host='0.0.0.0', port=8080)  # 启动flask
---
+++ wxsb.py
@@ -0,0 +1,101 @@
+import wxpy
+import time
+
+class sbcw():
+
+    def send_msg(self):
+        bot = wxpy.Bot()
+
+        found1 = bot.groups().search(u'药小招全国群')
+        obj1 = found1[0]
+        print(obj1)
+
+        found2 = bot.groups().search(u'佛山药戴群')
+        obj2 = found2[0]
+        print(obj2)
+
+        found3 = bot.groups().search(u'钢材塑料贸易群3')
+        obj3 = found3[0]
+        print(obj3)
+
+        found4 = bot.groups().search(u'广东第三终端合作交流群1')
+        obj4 = found4[0]
+        print(obj4)
+
+        found5 = bot.groups().search(u'大佛山工商会计交流1群')
+        obj5 = found5[0]
+        print(obj5)
+
+        found6 = bot.groups().search(u'恋爱要趁早')
+        obj6 = found6[0]
+        print(obj6)
+
+        found7 = bot.groups().search(u'2020中国医疗器械论坛群')
+        obj7 = found7[0]
+        print(obj7)
+
+        found8 = bot.groups().search(u'【饲料联盟】45群')
+        obj8 = found8[0]
+        print(obj8)
+
+        found9 = bot.groups().search(u'医药暴富群')
+        obj9 = found9[0]
+        print(obj9)
+
+        found10 = bot.groups().search(u'院长是我二大爷')
+        obj10 = found10[0]
+        print(obj10)
+
+        found11 = bot.groups().search(u'库房空着等我压')
+        obj11 = found11[0]
+        print(obj11)
+
+        found13 = bot.groups().search(u'20/03 20年 医疗新政策')
+        obj13 = found13[0]
+        print(obj13)
+
+        found14 = bot.groups().search(u'3048 医疗职位分享群')
+        obj14 = found14[0]
+        print(obj14)
+        
+        found16 = bot.groups().search(u'禾嘉医疗技术交流群')
+        obj16 = found16[0]
+        print(obj16)
+
+        found17 = bot.groups().search(u'欣欣小超市')
+        obj17 = found17[0]
+        print(obj17)
+
+        found18 = bot.groups().search(u'【广东】医药代表交流群')
+        obj18 = found18[0]
+        print(obj18)
+
+        
+        found19 = bot.groups().search(u'国际陨石奇石交流群')
+        obj19 = found19[0]
+        print(obj19)
+
+        found20 = bot.groups().search(u'依云水岸买买买吃货群')
+        obj20 = found20[0]
+        print(obj20)
+
+        found21 = bot.groups().search(u'依云水岸42栋业主群')
+        obj21 = found21[0]
+        print(obj21)
+
+        us = [obj20,obj21]
+        # users = [obj1,obj2,obj3,obj4,obj5,obj6,obj7,obj8,obj9,obj10,obj11,obj13,obj14,obj16,obj17,obj18,obj19]
+        # users = [obj10,obj11]
+        # txt = open('wcso.txt', 'r', encoding='utf-8')
+        # t = txt.read()
+        # print(t)
+
+        tc = open('wxcw.txt', 'r', encoding='utf-8')
+        c = tc.read()
+        print(c)
+        try:
+            for u in us:
+                u.send(c)
+            # for user in users:
+                # user.send(t)
+        except:pass
